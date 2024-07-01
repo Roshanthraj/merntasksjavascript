59e13ce1dd5add21a6c70c2aedfc2d375318f493
Roshanth <roshanthraj@gmail.com>
2024-07-01 10:54:38 +0530

    javascript tasks

---
+++ javascripttasks/task3script.js
@@ -0,0 +1,16 @@
+function multiply() {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  let result = num1 * num2;
+  document.querySelector(".result").innerHTML = "The Result Is:<br>" + result;
+}
+function divide() {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  let result = num1 / num2;
+  document.querySelector(".result").innerHTML = "The Result Is:<br>" + result;
+}
